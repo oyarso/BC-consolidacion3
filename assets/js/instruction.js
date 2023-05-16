4910e4e2eef23c6749a4a7151b4e4f80a78a1f3a
oyarso <sam.copae@gmail.com>
2023-05-16 13:33:54 -0400

    first commit

---
+++ assets/js/instruction.js
@@ -0,0 +1,25 @@
+function myFunction1()
+{
+    document.getElementById('foo1').setAttribute("class", "style1");
+}
+
+function myFunction2()
+{
+    document.getElementById('foo2').setAttribute("class", "style1");
+}
+
+function myFunction3()
+{
+    document.getElementById('foo3').setAttribute("class", "style1");
+}
+
+function myFunction4()
+{
+    document.getElementById('foo4').setAttribute("class", "style1");
+}
+
+function myFunction5()
+{
+    document.getElementById('foo5').setAttribute("class", "style1");
+}
+
